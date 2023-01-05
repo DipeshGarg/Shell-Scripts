98b0d2ea63a69487b0c256fe54c75de67dab7753
DipeshGarg <109458091+DipeshGarg@users.noreply.github.com>
2023-01-05 12:29:36 +0530

    Update git-tools-2.sh

--- git-tools-2.sh
+++ git-tools-2.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+{
+normal=`echo -en "\e[0m"`
+red=`echo -en "\e[31m"`
+green=`echo -en "\e[32m"`
+orange=`echo -en "\e[33m"`
+blue=`echo -en "\e[34m"`
+purple=`echo -en "\e[35m"`
+aqua=`echo -en "\e[36m"`
+gray=`echo -en "\e[37m"`
+lightgreen=`echo -en "\e[92m"`
+lightyellow=`echo -en "\e[93m"`
+lightblue=`echo -en "\e[94m"`
+lightpurple=`echo -en "\e[95m"`
+lightaqua=`echo -en "\e[96m"`
+}
+
 url=(
 'https://github.com/commixproject/commix.git'
 'https://github.com/epi052/feroxbuster.git'
@@ -24,9 +40,10 @@ url=(
 'https://github.com/hahwul/XSpear.git'
 'https://github.com/chenjj/CORScanner.git'
 'https://github.com/s0md3v/Corsy.git'
-'https://github.com/lc/gau.git'
+'https://github.com/projectdiscovery/chaos-client.git'
 'https://github.com/michenriksen/aquatone.git'
 'https://github.com/projectdiscovery/uncover.git'
+'https://github.com/lc/gau.git'
 'https://github.com/projectdiscovery/chaos-client.git'
 'https://github.com/projectdiscovery/shuffledns.git'
 'https://github.com/s0md3v/XSStrike.git'
@@ -35,14 +52,31 @@ url=(
 'https://github.com/Dewalt-arch/pimpmykali.git'
 'https://github.com/r0oth3x49/ghauri.git'
 'https://github.com/Dheerajmadhukar/4-ZERO-3.git'
-'https://github.com/initstring/cloud_enum.git'
  #'https://github.com/tomnomnom/gf.git'
 'https://github.com/1ndianl33t/Gf-Patterns.git'
+'https://github.com/initstring/cloud_enum.git'
+'https://github.com/GerbenJavado/LinkFinder.git'
 'https://github.com/urbanadventurer/WhatWeb.git'
 'https://github.com/D4Vinci/Clickjacking-Tester.git'
-'https://github.com/GerbenJavado/LinkFinder.git'
 'https://github.com/tomnomnom/qsreplace.git'
 )
+
+real() {
+if [ $i -lt 10 ]
+then
+  echo -e " $i. \c"
+  echo -e "$(echo "${url[i]}" | cut -d "." -f2 | cut -d "/" -f3) \c"
+  i=$(($i+1))
+  echo -e "\t\t $i. \c"
+  echo "$(echo "${url[i]}" | cut -d "." -f2 | cut -d "/" -f3)"
+else
+  echo -e "$i. \c"
+  echo -e "$(echo "${url[i]}" | cut -d "." -f2 | cut -d "/" -f3) \c"
+  i=$(($i+1))
+  echo -e "\t\t$i. \c"
+  echo "$(echo "${url[i]}" | cut -d "." -f2 | cut -d "/" -f3)"
+fi
+}
 
 n=`echo "${#url[@]}"`
 echo -e "List of tools which can be installed...\n"
@@ -52,11 +86,17 @@ do
   then
    echo -e "\n# Github Tools below..."
   fi
-  echo -e "$i. \c"
-  echo -e "$(echo "${url[i]}" | cut -d "." -f2 | cut -d "/" -f3) \c"
-  i=$(($i+1))
-  echo -e "\t\t$i. \c"
-  echo "$(echo "${url[i]}" | cut -d "." -f2 | cut -d "/" -f3)"
+
+  if [ $(($i/2)) -eq 0 ]
+  then
+#    ${purple}
+    real
+#    ${normal}
+  else
+#    ${grey}
+    real
+#    ${normal}
+  fi
 done
 
 echo -e "\nSyntax: 3,2,4 or exit"
