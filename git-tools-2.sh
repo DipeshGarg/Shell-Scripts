ab0ed6bbad7f98376f66008018de5528ec767f77
DipeshGarg <109458091+DipeshGarg@users.noreply.github.com>
2023-01-11 20:55:44 +0530

    Update git-tools-2.sh

--- git-tools-2.sh
+++ git-tools-2.sh
@@ -1,29 +1,15 @@
 #!/bin/bash
 
+#Colour-Codes
 {
 normal=`echo -en "\e[0m"`
 red=`echo -en "\e[31m"`
 green=`echo -en "\e[32m"`
 orange=`echo -en "\e[33m"`
 blue=`echo -en "\e[34m"`
+lightgreen=`echo -en "\e[92m"`
 purple=`echo -en "\e[35m"`
 aqua=`echo -en "\e[36m"`
-gray=`echo -en "\e[37m"`
-lightgreen=`echo -en "\e[92m"`
-lightyellow=`echo -en "\e[93m"`
-lightblue=`echo -en "\e[94m"`
-lightpurple=`echo -en "\e[95m"`
-lightaqua=`echo -en "\e[96m"`
-#----------------
-# red='\e[31m'
-# green='\e[32m'
-# blue='\e[34m'
-# cyan='\e[96m'
-# ltcyan='\e[96m'
-# yellow='\e[33m'
-# black='\e[38;5;016m'
-# bluebg='\e[48;5;038m'${black}
-# normal='\e[0m'
 }
 
 url=(
@@ -31,6 +17,7 @@ url=(
 'https://github.com/epi052/feroxbuster.git'
 'https://github.com/ffuf/ffuf.git'
 'https://github.com/haccer/subjack.git'
+#'https://github.com/jordansissel/xdotool.git'
 'https://github.com/hakluke/hakrawler.git'
 'https://github.com/jaeles-project/gospider.git'
 'https://github.com/java-decompiler/jd-gui.git'
@@ -42,7 +29,8 @@ url=(
 'https://github.com/sqlmapproject/sqlmap.git'
 'https://github.com/projectdiscovery/httpx-toolkit.git'
 'https://github.com/OWASP/amass.git'
-'https://github.com/tomnomnom/assetfinder.git' #last one
+'https://github.com/tomnomnom/assetfinder.git'
+# Github Tools
 'https://github.com/0xInfection/XSRFProbe.git'
 'https://github.com/codingo/NoSQLMap.git'
 'https://github.com/defparam/smuggler.git'
@@ -50,11 +38,9 @@ url=(
 'https://github.com/hahwul/XSpear.git'
 'https://github.com/chenjj/CORScanner.git'
 'https://github.com/s0md3v/Corsy.git'
-'https://github.com/projectdiscovery/chaos-client.git'
 'https://github.com/michenriksen/aquatone.git'
-'https://github.com/projectdiscovery/uncover.git'
-'https://github.com/lc/gau.git'
 'https://github.com/projectdiscovery/chaos-client.git'
+'https://github.com/projectdiscovery/uncover.git'
 'https://github.com/projectdiscovery/shuffledns.git'
 'https://github.com/s0md3v/XSStrike.git'
 'https://github.com/sensepost/gowitness.git'
@@ -62,53 +48,62 @@ url=(
 'https://github.com/Dewalt-arch/pimpmykali.git'
 'https://github.com/r0oth3x49/ghauri.git'
 'https://github.com/Dheerajmadhukar/4-ZERO-3.git'
+'https://github.com/initstring/cloud_enum.git'
  #'https://github.com/tomnomnom/gf.git'
 'https://github.com/1ndianl33t/Gf-Patterns.git'
-'https://github.com/initstring/cloud_enum.git'
-'https://github.com/GerbenJavado/LinkFinder.git'
 'https://github.com/urbanadventurer/WhatWeb.git'
 'https://github.com/D4Vinci/Clickjacking-Tester.git'
+'https://github.com/GerbenJavado/LinkFinder.git'
 'https://github.com/tomnomnom/qsreplace.git'
+# GO Tools
+'https://github.com/lc/subjs.git'
+'https://github.com/lc/gau.git'
+'github.com/hahwul/dalfox/v2@latest'
 )
 
-real() {
-if [ $i -lt 10 ]
-then
-  echo -e " $i. \c"
-  echo -e "$(echo "${url[i]}" | cut -d "." -f2 | cut -d "/" -f3) \c"
-  i=$(($i+1))
-  echo -e "\t\t $i. \c"
-  echo "$(echo "${url[i]}" | cut -d "." -f2 | cut -d "/" -f3)"
-else
-  echo -e "$i. \c"
-  echo -e "$(echo "${url[i]}" | cut -d "." -f2 | cut -d "/" -f3) \c"
-  i=$(($i+1))
-  echo -e "\t\t$i. \c"
-  echo "$(echo "${url[i]}" | cut -d "." -f2 | cut -d "/" -f3)"
-fi
-}
+aptcount=16 ;
+gocount=38 ;
 
-n=`echo "${#url[@]}"`
-echo -e "List of tools which can be installed...\n"
-for ((i=0; i<$n ;i++))
-do
-  if [ $i -eq 16 ]
-  then
-   echo -e "\n# Github Tools below..."
-  fi
-  # a=`echo $(expr $i % 2)`
-  # echo "value of a "=$a
-  # if [ $a -eq 0 ]
-  # then
-    echo -ne "${green}"
-    real
-    echo -ne "${normal}"
-  # else
-  #   echo -ne "${gray}"
-  #   real
-  #   echo -ne "${normal}"
-  # fi
-done
+echo "
+List of tools which can be installed...
+
+$normal# Kali Tools Below...$red
+ 0. commix               1. feroxbuster           2. ffuf
+ 3. subjack              4. hakrawler             5. gospider
+ 6. jd-gui               7. dnsx                  8. nuclei
+ 9. subfinder           10. masscan              11. jadx
+12. sqlmap              13. httpx-toolkit        14. amass
+15. assetfinder
+
+$normal# Github Tools Below...$green
+16. XSRFProbe           17. NoSQLMap             18. smuggler
+19. EyeWitness          20. XSpear               21. CORScanner
+22. Corsy               23. aquatone             24. chaos-client
+25. uncover             26. shuffledns           27. XSStrike
+28. gowitness           29. SSRFmap              30. pimpmykali
+31. ghauri              32. 4-ZERO-3             33. cloud_enum
+34. Gf-Patterns         35. WhatWeb              36. Clickjacking-Tester
+37. LinkFinder          38. qsreplace
+
+$normal# Go Tools Below...$aqua
+39. subjs               40. gau                  41. dalfox
+$normal "
+# n=`echo "${#url[@]}"`
+# echo -e "List of tools which can be installed...\n"
+# for ((i=0; i<$n ;i++))
+# do
+#   if [ $i -eq $aptcount ]
+#   then
+#    echo -e "\n# Github Tools below..."
+#  elif [[ $i -gt $gocount ]]; then
+#    echo -e "\n# Go Tools below..."
+#   fi
+#   echo -e "$i. \c"
+#   echo -e "$(echo "${url[i]}" | cut -d "." -f2 | cut -d "/" -f3) \c"
+#   i=$(($i+1))
+#   echo -e "\t\t$i. \c"
+#   echo "$(echo "${url[i]}" | cut -d "." -f2 | cut -d "/" -f3)"
+# done
 
 echo -e "\nSyntax: 3,2,4 or exit"
 echo "Which of these tools you want to install ?"
@@ -121,10 +116,14 @@ echo -e "\nStarted installing the tools...\n"
 for ((i=0; i<n; i++))
 do
   name=`echo $(echo ${url[${ans[i]}]} | cut -d "." -f2 | cut -d "/" -f3 )`
-  if [ ${ans[i]} -lt 16 ]
-  then
+  if [ ${ans[i]} -lt $aptcount ]; then
+    echo "sudo apt-get install $name"
     echo "$(sudo apt-get install $name -y )"
+  elif [[ ${ans[i]} -gt $gocount ]]; then
+    echo "go install ${url[${ans[i]}]}"
+    echo "$(go install ${url[${ans[i]}]} )"
   else
+    echo "sudo git clone ${url[${ans[i]}]}"
     echo "$(sudo git clone ${url[${ans[i]}]})"
   fi
 done
